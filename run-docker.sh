e28790ca1f333292abb0b1d741e593d5af599012
Stephen Donner <stephen.donner@gmail.com>
2016-05-06 13:58:03 -0700

    Add commands which need dynamically passed-in container ID/name

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 docker run -u zap -p 8080:8080 -d owasp/zap2docker-stable zap.sh -daemon -port 8080 -host 0.0.0.0 -config api.disablekey=true
+
+# next, we need to do a docker exec [container ID or name] zap-cli open-url
+# XXX - FIXME: we need to dynamically pass in the container ID / name
+
+# docker exec [container ID or name] zap-cli active-scan 'https://www.allizom.org'
+# XXX - FIXME: we need to dynamically pass in the container ID / name
+
+# docker logs [container ID or name]
+# XXX - FIXME: we need to dynamically pass in the container ID / name
