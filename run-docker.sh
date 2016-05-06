fe78fe1f2d671ace1d80fb6136fc231746da462b
Stephen Donner <stephen.donner@gmail.com>
2016-05-06 14:51:33 -0700

    Add sleep; find + use container ID dynamically

--- run-docker.sh
+++ run-docker.sh
@@ -2,11 +2,14 @@
 
 docker run -u zap -p 8080:8080 -d owasp/zap2docker-stable zap.sh -daemon -port 8080 -host 0.0.0.0 -config api.disablekey=true
 
-# next, we need to do a docker exec [container ID or name] zap-cli open-url
-# XXX - FIXME: we need to dynamically pass in the container ID / name
+# XXX - FIXME: should smartly listen for ZAP-readiness, rather than hard-code a sleep
+sleep 10
 
-# docker exec [container ID or name] zap-cli active-scan 'https://www.allizom.org'
-# XXX - FIXME: we need to dynamically pass in the container ID / name
+docker exec $(docker ps -lq) zap-cli open-url https://www.allizom.org/en-US/firefox/
+
+docker exec $(docker ps -lq) zap-cli active-scan https://www.allizom.org/en-US/firefox/
 
 # docker logs [container ID or name]
-# XXX - FIXME: we need to dynamically pass in the container ID / name
+docker logs $(docker ps -lq)
+
+docker stop $(docker ps -lq)
