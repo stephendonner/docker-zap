d7ac62580c2bf3b7747677fdf15fdff427cd85a9
Stephen Donner <stephen.donner@gmail.com>
2016-06-28 14:50:10 -0700

    Update and use a different port
    Now matches the TCP port from my Jenkins-integrations blog post: https://blog.mozilla.org/webqa/2016/06/28/dockerized-owasp-zap-security-scanning-in-jenkins-part-two/

--- run-docker.sh
+++ run-docker.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-CONTAINER_ID=$(docker run -u zap -p 8090:8090 -d owasp/zap2docker-weekly zap.sh -daemon -port 8090 -host 0.0.0.0 -config api.disablekey=true)
+CONTAINER_ID=$(docker run -u zap -p 2375:2375 -d owasp/zap2docker-weekly zap.sh -daemon -port 2375 -host 127.0.0.1 -config api.disablekey=true)
 
 # XXX - FIXME: should smartly listen for ZAP-readiness, rather than hard-code a sleep
 sleep 20
 
-docker exec $CONTAINER_ID zap-cli open-url https://www.allizom.org/en-US/firefox/
+docker exec $CONTAINER_ID zap-cli -p 2375 open-url https://www.allizom.org/en-US/firefox/
 
-docker exec $CONTAINER_ID zap-cli spider https://www.allizom.org/en-US/firefox/
+docker exec $CONTAINER_ID zap-cli -p 2375 spider https://www.allizom.org/en-US/firefox/
 
 # XXX - FIXME: not only should the hard-coded host be removed, but we should pass in more-aggressive active-scan options, here
-docker exec $CONTAINER_ID zap-cli active-scan https://www.allizom.org/en-US/firefox/
+docker exec $CONTAINER_ID zap-cli -p 2375 active-scan https://www.allizom.org/en-US/firefox/
 
 # docker logs [container ID or name]
 docker logs $CONTAINER_ID
