108944cdc52818b54ebde63fce5f6f50bdc06dab
Stephen Donner <stephen.donner@gmail.com>
2016-08-09 16:21:42 -0700

    Remove hard-coded site; abstract into TARGET_URL

--- run-docker.sh
+++ run-docker.sh
@@ -2,15 +2,18 @@
 
 CONTAINER_ID=$(docker run -u zap -p 2375:2375 -d owasp/zap2docker-weekly zap.sh -daemon -port 2375 -host 127.0.0.1 -config api.disablekey=true)
 
+# the target URL for ZAP to scan
+TARGET_URL=""
+
 # XXX - FIXME: should smartly listen for ZAP-readiness, rather than hard-code a sleep
 sleep 20
 
-docker exec $CONTAINER_ID zap-cli -p 2375 open-url https://www.allizom.org/en-US/firefox/
+docker exec $CONTAINER_ID zap-cli -p 2375 open-url $TARGET_URL
 
-docker exec $CONTAINER_ID zap-cli -p 2375 spider https://www.allizom.org/en-US/firefox/
+docker exec $CONTAINER_ID zap-cli -p 2375 spider $TARGET_URL
 
 # XXX - FIXME: not only should the hard-coded host be removed, but we should pass in more-aggressive active-scan options, here
-docker exec $CONTAINER_ID zap-cli -p 2375 active-scan https://www.allizom.org/en-US/firefox/
+docker exec $CONTAINER_ID zap-cli -p 2375 active-scan $TARGET_URL
 
 # docker logs [container ID or name]
 docker logs $CONTAINER_ID
