2459a1873d9ab21849281a57ae43da79635a3ea6
Stephen Donner <stephen.donner@gmail.com>
2016-08-31 16:13:43 -0700

    Remove hardcoded sleep and poll/wait for ZAP-API readiness - fixes https://github.com/stephendonner/docker-zap/issues/1 (#9)
    Now we poll ZAP-API readiness, up to 60 seconds, before we open-url.  And return "ZAP is not running", if it's not ready (although we continue on, later).

--- run-docker.sh
+++ run-docker.sh
@@ -5,10 +5,7 @@ CONTAINER_ID=$(docker run -u zap -p 2375:2375 -d owasp/zap2docker-weekly zap.sh 
 # the target URL for ZAP to scan
 TARGET_URL=""
 
-# XXX - FIXME: should smartly listen for ZAP-readiness, rather than hard-code a sleep
-sleep 20
-
-docker exec $CONTAINER_ID zap-cli -p 2375 open-url $TARGET_URL
+docker exec $CONTAINER_ID zap-cli -p 2375 status -t 60 && docker exec $CONTAINER_ID zap-cli -p 2375 open-url $TARGET_URL
 
 docker exec $CONTAINER_ID zap-cli -p 2375 spider $TARGET_URL
 
