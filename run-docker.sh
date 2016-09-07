80836b4e98492b6cbf82661cc0557986f83cac36
Stephen Donner <stephen.donner@gmail.com>
2016-09-07 00:23:00 -0700

    Should fix Issue 12 - add in ATTACK mode to ZAP on startup

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CONTAINER_ID=$(docker run -u zap -p 2375:2375 -d owasp/zap2docker-weekly zap.sh -daemon -port 2375 -host 127.0.0.1 -config api.disablekey=true)
+CONTAINER_ID=$(docker run -u zap -p 2375:2375 -d owasp/zap2docker-weekly zap.sh -daemon -port 2375 -host 127.0.0.1 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack)
 
 # the target URL for ZAP to scan
 TARGET_URL=$1
